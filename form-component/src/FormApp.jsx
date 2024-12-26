f52c3f446b58f0313cd7ea6756a53508391df9d1
ProgramNS <blise01@hotmail.com>
2024-12-26 20:32:46 +0700

    Belajar membuat form component

---
+++ form-component/src/FormApp.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import FormComponent from "./FormComponent";
+
+const root = createRoot(document.getElementById('root'));
+root.render(<FormComponent/>);
